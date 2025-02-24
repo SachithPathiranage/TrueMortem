e2681d58b1f99c42cf86614780c08f8cfdc0c7df
Ashan-KS <ashansuriyarachchi@gmail.com>
2025-02-24 06:45:13 +0530

    Built the AboutUs page according to wireframe

--- Application/frontend/TrueMortem/src/components/Navbar.jsx
+++ Application/frontend/TrueMortem/src/components/Navbar.jsx
@@ -1,85 +1,59 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Menu, X } from "lucide-react"; // Icons for the mobile menu
+import { BsChatLeftText } from "react-icons/bs";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
   return (
-    <header className="absolute top-0 left-0 w-full bg-[#f6f6f6]">
-      <nav className="flex justify-around items-center">
-        {/* Logo (Left) */}
-        <a href="/" className="shrink-0">
-          <img
-            src="/trueMortem-full.png"
-            alt="TrueMortem Logo"
-            className="w-auto h-[23px] sm:h-[20px] md:h-[30px] grow-0"
-          />
+    <header className="flex justify-between items-center w-full bg-[#f6f6f6] h-24 max-w-[1240px] mx-auto px-4">
+      {/* Logo (Left) */}
+      <a href="/" className="shrink-0 flex">
+        <img
+          src="/trueMortem.png"
+          alt="TrueMortem Logo"
+          className="w-auto h-[23px] grow-0 my-auto"
+        />
+        <a href="/" className="px-1.5 mx-auto my-auto text-xl">
+          TrueMortem
         </a>
+      </a>
 
-        {/* Desktop Menu (Middle) */}
-        <ul className="flex-1 flex justify-center grow-0 items-center gap-3">
-          <li>
-            <Link
-              to="/"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/predict"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              Predict
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+      {/* Desktop Menu (Middle) */}
+      <ul className="flex-1 flex justify-center grow-0 items-center gap-1">
+        <li className="flex items-center justify-center text-sm w-[70px] h-[35px] bg-[#efefef] rounded-[25px] text-[#737373] hover:text-[#494646] hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="flex items-center justify-center text-sm w-[70px] h-[35px] bg-[#efefef] rounded-[25px] text-[#737373] hover:text-[#494646] hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/predict">Predict</Link>
+        </li>
+        <li className="flex items-center justify-center text-sm w-[70px] h-[35px] bg-[#efefef] rounded-[25px] text-[#737373] hover:text-[#494646] hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="flex items-center justify-center text-sm w-[70px] h-[35px] bg-[#efefef] rounded-[25px] text-[#737373] hover:text-[#494646] hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
 
-        {/* Right Section (Chat, Bell, Profile) */}
-        <ul className="flex-1 flex justify-center grow-0 items-center gap-4">
-          <li>
-            <Link
-              to="/chat"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              Chat
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/notifications"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              Bell
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/profile"
-              className="px-4 py-2 bg-white rounded-lg text-black hover:bg-gray-100 transition"
-            >
-              DP
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      {/* Right Section (Chat, Bell, Profile) */}
+      <ul className="flex-1 flex justify-center grow-0 items-center gap-1">
+        <li className="shadow-lg flex items-center justify-center text-[20px] w-[55px] h-[50px] bg-[#efefef] rounded-4xl text-black hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/chat">
+            <BsChatLeftText />
+          </Link>
+        </li>
+        <li className="shadow-lg flex items-center justify-center text-[25px] w-[55px] h-[50px] bg-[#efefef] rounded-[25px] text-black hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/notifications">
+            <IoMdNotificationsOutline />
+          </Link>
+        </li>
+        <li className="shadow-lg flex items-center justify-center text-[25px] w-[55px] h-[50px] bg-[#efefef] rounded-[25px] text-black hover:bg-[#e2e0e0] transition align-middle">
+          <Link to="/profile">
+            <CgProfile />
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 };
--- Application/frontend/TrueMortem/src/pages/About.jsx
+++ Application/frontend/TrueMortem/src/pages/About.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-      <div className='mt-30'>
-        <h1>ℹ️ About Us</h1>
-        <p>Learn more about our company and team.</p>
+  return (
+    <div className="w-full h-[calc(100vh-13vh)] flex items-center justify-center">
+      <div className="flex flex-col items-center text-center justify-center max-w-[1240px] mx-auto my-auto">
+        <h1 className="text-[20px] mt-1">About Us</h1>
+        <h1 className="text-[50px] font-bold max-w-[60%] mt-7">
+          We combine the precision of AI with expertise in forensic science.
+        </h1>
+        <p className="max-w-[55%] text-[17px] mt-7">
+          Our philosophy is simple: harness the power of AI to empower
+          professionals with fast, reliable, and accurate predictions.
+        </p>
       </div>
-    );
-  };
-  
-  export default About;
+    </div>
+  );
+};
+
+export default About;
